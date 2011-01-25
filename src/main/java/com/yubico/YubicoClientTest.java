cafb1e94b2739a9fa888e6bbab3b1364a04fdfed
Fredrik Thulin <fredrik@yubico.com>
2011-01-25 10:56:39 +0100

    Add minimal test case.

---
+++ src/main/java/com/yubico/YubicoClientTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.yubico;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author ft
+ *
+ */
+public class YubicoClientTest {
+
+	/**
+	 * Test method for {@link com.yubico.YubicoClient#YubicoClient(int)}.
+	 */
+	@SuppressWarnings("static-access")
+	@Test
+	public void testYubicoClient() {
+		YubicoClient c = new YubicoClient(4711);
+		
+		assertTrue(c.YUBICO_AUTH_SRV_URL.contains("yubico.org"));
+	}
+
+	/**
+	 * Test method for {@link com.yubico.YubicoClient#getId()}.
+	 */
+	@Test
+	public void testGetId() {
+		YubicoClient c = new YubicoClient(4711);
+
+		assertEquals(4711, c.getId());
+	}
+
+}

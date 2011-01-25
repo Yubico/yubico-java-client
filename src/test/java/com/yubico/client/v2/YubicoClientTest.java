648c3105eb8885e24c39a455ad37a8daf696fbe1
Linus Widströmer <linus.widstromer@it.su.se>
2011-01-25 16:16:34 +0100

    Updated test to reflect the new code

--- src/test/java/com/yubico/client/v2/YubicoClientTest.java
+++ src/test/java/com/yubico/client/v2/YubicoClientTest.java
@@ -1,5 +1,10 @@
 package com.yubico.client.v2;
 
+import com.yubico.client.v2.impl.YubicoClientImpl;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Created by IntelliJ IDEA.
  * User: lwid
@@ -8,4 +13,35 @@ package com.yubico.client.v2;
  * To change this template use File | Settings | File Templates.
  */
 public class YubicoClientTest {
+
+    private YubicoClient client=null;
+
+    @Before
+    public void setup() {
+        client=new YubicoClientImpl();
+    }
+
+    @Test
+    public void verifyContruct() {
+        assertTrue(client instanceof YubicoClientImpl);
+    }
+
+    @Test
+    public void testBadOTP() {
+        String otp="kaka";
+        YubicoResponse response = client.verify(4711, otp);
+        assertNotNull(response);
+        assertTrue(response.getStatus() == YubicoResponseStatus.BAD_OTP);
+        assertEquals(otp, response.getOtp());
+    }
+
+    @Test
+    public void testReplayedOTP() {
+        String otp="cccccccfhcbelrhifnjrrddcgrburluurftrgfdrdifj";
+        YubicoResponse response = client.verify(4711, otp);
+        assertNotNull(response);
+        assertTrue(response.getStatus() == YubicoResponseStatus.REPLAYED_OTP);
+        assertEquals(otp, response.getOtp());
+    }
+
 }

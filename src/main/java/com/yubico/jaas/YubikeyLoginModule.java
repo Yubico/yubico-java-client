2cad2ec2852bb89f3ab2bb7bbbbd41e15d80c040
Fredrik Thulin <fredrik@yubico.com>
2011-02-01 22:54:37 +0100

    Check for correct YubiKey owner.
    We're using a simple key->value text file for now.
    
    Supports auto-provisioning (adding new YubiKeys to the file as
    belonging to whoever logs in with them first).

--- src/main/java/com/yubico/jaas/YubikeyLoginModule.java
+++ src/main/java/com/yubico/jaas/YubikeyLoginModule.java
@@ -29,10 +29,14 @@
  */
 package com.yubico.jaas;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import javax.security.auth.Subject;
 import javax.security.auth.callback.Callback;
@@ -46,10 +50,6 @@ import javax.security.auth.spi.LoginModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
 import com.yubico.YubicoClient;
 
 /**
@@ -62,7 +62,10 @@ import com.yubico.YubicoClient;
 public class YubikeyLoginModule implements LoginModule {
 
 	/* Options */
-	public static final String OPTION_YUBICO_CLIENT_ID = "clientId";
+	public static final String OPTION_YUBICO_CLIENT_ID			= "clientId";
+	public static final String OPTION_YUBICO_ID2NAME_TEXTFILE	= "id2name_textfile";
+	public static final String OPTION_YUBICO_VERIFY_YK_OWNER	= "verify_yubikey_owner";
+	public static final String OPTION_YUBICO_AUTO_PROVISION		= "auto_provision_owner";
 
 	/* JAAS stuff */
 	private Subject subject;
@@ -71,6 +74,10 @@ public class YubikeyLoginModule implements LoginModule {
 	/* YubicoClient settings */
 	private Integer clientId;
 	private YubicoClient yc;
+
+	private String id2name_textfile;
+	private boolean verify_yubikey_owner;
+	private boolean auto_provision_owners;
 
 	private final Logger log = LoggerFactory.getLogger(YubikeyLoginModule.class);
 
@@ -103,9 +110,30 @@ public class YubikeyLoginModule implements LoginModule {
 		this.subject = newSubject;
 		this.callbackHandler = newCallbackHandler;
 
+		/* Yubico verification client */
 		this.clientId = Integer.parseInt(options.get(OPTION_YUBICO_CLIENT_ID).toString());
-
 		this.yc = new YubicoClient(this.clientId);
+
+		/* id2name text file */
+		if (options.get(OPTION_YUBICO_ID2NAME_TEXTFILE) != null) {
+			this.id2name_textfile = options.get(OPTION_YUBICO_ID2NAME_TEXTFILE).toString();
+		}
+
+		/* Verify yubikey owner */
+		this.verify_yubikey_owner = true;
+		if (options.get(OPTION_YUBICO_VERIFY_YK_OWNER) != null) {
+			if ("false".equals(options.get(OPTION_YUBICO_VERIFY_YK_OWNER).toString())) {
+				this.verify_yubikey_owner = false;
+			}
+		}	
+
+		/* Auto provision owners */
+		this.auto_provision_owners = false;
+		if (options.get(OPTION_YUBICO_AUTO_PROVISION) != null) {
+			if ("true".equals(options.get(OPTION_YUBICO_AUTO_PROVISION).toString())) {
+				this.auto_provision_owners = true;
+			}
+		}	
 	}
 
 	/* (non-Javadoc)
@@ -120,9 +148,9 @@ public class YubikeyLoginModule implements LoginModule {
 		if (callbackHandler == null) {
 			throw new LoginException("No callback handler available in login()");
 		}
-		
+
 		List<String> otps = get_tokens(nameCb);
-		
+
 		for (String otp : otps) {
 			log.trace("Checking OTP {}", otp);
 
@@ -145,8 +173,52 @@ public class YubikeyLoginModule implements LoginModule {
 	}
 
 	private boolean is_right_user(String username, String publicId) {
-	    //log.trace("Check if YubiKey {} belongs to user {}", publicId, username);
-	    return true;
+		log.debug("Check if YubiKey {} belongs to user {}", publicId, username);
+		if (this.id2name_textfile == null) {
+			log.debug("No id2name configuration. Defaulting to {}.", this.verify_yubikey_owner);
+			return this.verify_yubikey_owner;
+		}
+
+		Scanner sc = null;
+		File file = new File(this.id2name_textfile);
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				if (line.startsWith("yk." + publicId + ".user")) {
+					String ykuser = line.split("=")[1].trim();
+					
+					if (! ykuser.equals(username)) {
+						log.info("YubiKey " + publicId + " registered to user " + ykuser
+								+ ", not " + username);
+						return false;
+					}
+					return true;
+				}
+			}
+		} catch (FileNotFoundException ex) {
+			log.error("Yubikey to username textfile {} not found", this.id2name_textfile);
+			return false;
+		} finally {
+			if (sc != null) {
+				sc.close();	
+			}
+		}	    
+
+		if (this.auto_provision_owners) {
+			log.info("Registering new YubiKey " + publicId + " as belonging to {}", username);
+			
+			try {
+				FileWriter writer = new FileWriter(file, true);
+				writer.write("yk." + publicId + ".user = " + username
+						+ System.getProperty("line.separator"));
+				writer.close();	
+			} catch (IOException ex) {
+				log.error("Failed appending entry to file {}", this.id2name_textfile, ex);
+			}
+			return true;
+		}	    
+		return false;
 	}
 
 	private List<String> get_tokens(NameCallback nameCb) throws LoginException {
@@ -164,7 +236,7 @@ public class YubikeyLoginModule implements LoginModule {
 				 * in clear text, over the network.
 				 */
 				if (s.length() < 32) {
-					log.info("Skipping token, not a valid YubiKey OTP (too short, {} < 32)", s.length());
+					log.debug("Skipping token, not a valid YubiKey OTP (too short, {} < 32)", s.length());
 				} else {
 					result.add(s);
 				}

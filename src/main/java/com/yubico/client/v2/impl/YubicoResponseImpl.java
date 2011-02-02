9a37edd1d6337755d9ced443502454c5f3426689
Linus Widströmer <linus.widstromer@it.su.se>
2011-02-02 11:58:10 +0100

    Adding a copyright header to the classes

--- src/main/java/com/yubico/client/v2/impl/YubicoResponseImpl.java
+++ src/main/java/com/yubico/client/v2/impl/YubicoResponseImpl.java
@@ -8,13 +8,37 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-/**
- * Created by IntelliJ IDEA.
- * User: lwid
- * Date: 1/25/11
- * Time: 3:11 PM
- * To change this template use File | Settings | File Templates.
- */
+/* Copyright (c) 2011, Linus Widströmer.  All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+  
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+  
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following
+     disclaimer in the documentation and/or other materials provided
+     with the distribution.
+ 
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+   ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+   TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+   THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+   SUCH DAMAGE.
+ 
+   Written by Linus Widströmer <linus.widstromer@it.su.se>, January 2011.
+*/
+
 public class YubicoResponseImpl implements YubicoResponse {
 
     private String h;

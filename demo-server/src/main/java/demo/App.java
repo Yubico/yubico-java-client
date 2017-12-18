1c931b8996156b2e953330ca6ae60c831e410c27
TwinProduction <christian-ct@hotmail.com>
2017-12-17 21:31:31 -0500

    Refactored code to increase readability

--- demo-server/src/main/java/demo/App.java
+++ demo-server/src/main/java/demo/App.java
@@ -4,17 +4,20 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
+
 public class App extends Application<Config> {
-    @Override
-    public void initialize(Bootstrap<Config> bootstrap) {
-    }
-
-    @Override
-    public void run(Config config, Environment environment) throws Exception {
-        environment.jersey().register(new Resource());
-    }
-
-    public static void main(String... args) throws Exception {
-        new App().run(args);
-    }
+	
+	@Override
+	public void initialize(Bootstrap<Config> bootstrap) {}
+	
+	
+	@Override
+	public void run(Config config, Environment environment) throws Exception {
+		environment.jersey().register(new Resource());
+	}
+	
+	
+	public static void main(String... args) throws Exception {
+		new App().run(args);
+	}
 }

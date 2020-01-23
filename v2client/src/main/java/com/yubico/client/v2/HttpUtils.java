0efdaf184bf77bc458b04b53dc9ccc5ee5960f9b
Emil Lundberg <emil@yubico.com>
2020-01-23 16:47:57 +0100

    Make HttpUtils.toQueryString() encode keys too

--- v2client/src/main/java/com/yubico/client/v2/HttpUtils.java
+++ v2client/src/main/java/com/yubico/client/v2/HttpUtils.java
@@ -6,16 +6,19 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class HttpUtils {
+
     public static String toQueryString(Map<String, String> requestMap) throws UnsupportedEncodingException {
         StringBuilder paramStr = new StringBuilder();
         for(Map.Entry<String,String> entry : requestMap.entrySet()) {
             if(paramStr.length() > 0) {
                 paramStr.append("&");
             }
-            paramStr.append(entry.getKey()).append("=").append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8.name()));
+            paramStr
+                .append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8.name()))
+                .append("=")
+                .append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8.name()));
         }
         return paramStr.toString();
     }
-
 
 }
